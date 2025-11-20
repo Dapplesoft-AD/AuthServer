74b6ea9c2dfca7fa4570a0b2d118adf9e3a213d0
Hasan Uddin <abcnizamd@gmail.com>
2025-11-20 16:19:15 +0600

    🐛 config fixed

--- src/Infrastructure/Users/UserConfiguration.cs
+++ src/Infrastructure/Users/UserConfiguration.cs
@@ -14,10 +14,20 @@ internal sealed class UserConfiguration : IEntityTypeConfiguration<User>
 
         builder.Property(u => u.Email).HasMaxLength(150).IsRequired();
 
+        builder.Property(u => u.FullName).HasMaxLength(100).IsRequired();
+
         builder.Property(u => u.Phone).HasMaxLength(20);
 
         builder.Property(u => u.PasswordHash).HasMaxLength(255).IsRequired();
 
         builder.Property(u => u.IsEmailVerified).IsRequired();
+
+        builder.Property(u => u.IsMFAEnabled).IsRequired();
+
+        builder.Property(u => u.Status).IsRequired();
+
+        builder.Property(u => u.CreatedAt).IsRequired();
+
+        builder.Property(u => u.UpdatedAt).IsRequired();
     }
 }
