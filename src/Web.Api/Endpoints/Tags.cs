7117b7bb02f4db04c2b6f1b59ad7387379f781a8
Hasan Uddin <abcnizamd@gmail.com>
2025-11-25 20:38:35 +0600

    🐛🤦‍♂️Delete Operation Fixed and Tags added

--- src/Web.Api/Endpoints/Tags.cs
+++ src/Web.Api/Endpoints/Tags.cs
@@ -10,4 +10,5 @@ public static class Tags
     public const string Token = "Token";
     public const string UserLoginHistory = "UserLoginHistory";
     public const string UserProfile = "UserProfile";
+    public const string Businesses = "Businesses";
 }
