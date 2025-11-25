40c17fe6d9ff9a4c15c54d220eabb036e575c84b
Hasan Uddin <abcnizamd@gmail.com>
2025-11-25 15:45:31 +0600

    🚧 copied from current Dev to remove conflict_2

--- src/Web.Api/Endpoints/Tags.cs
+++ src/Web.Api/Endpoints/Tags.cs
@@ -5,7 +5,9 @@ public static class Tags
     public const string Users = "Users";
     public const string Todos = "todos";
     public const string Customers = "Customers";
-    public const string Businesses = "Businesses";
-    public const string BusinessMembers = "Business Members";
-    public const string Roles = "Roles";  
+    public const string EmailVerifications = "Email Verification";
+    public const string PasswordReset = "Password Reset";
+    public const string Token = "Token";
+    public const string UserLoginHistory = "UserLoginHistory";
+    public const string UserProfile = "UserProfile";
 }
