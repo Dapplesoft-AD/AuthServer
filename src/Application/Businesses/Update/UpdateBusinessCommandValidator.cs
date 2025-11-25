18c1771f87acd6f74eeecd6acf38915675c3712e
Hasan Uddin <abcnizamd@gmail.com>
2025-11-25 20:13:58 +0600

    formated

--- src/Application/Businesses/Update/UpdateBusinessCommandValidator.cs
+++ src/Application/Businesses/Update/UpdateBusinessCommandValidator.cs
@@ -7,8 +7,11 @@ public sealed class UpdateBusinessCommandValidator : AbstractValidator<UpdateBus
     public UpdateBusinessCommandValidator()
     {
         RuleFor(x => x.Id).NotEmpty().WithMessage("Business Id is required.");
+
         RuleFor(x => x.BusinessName).NotEmpty().WithMessage("Business Name is required.");
+
         RuleFor(x => x.IndustryType).NotEmpty().WithMessage("Industry Type is required.");
+
         RuleFor(x => x.Status)
             .IsInEnum()
             .WithMessage("Status must be either Active or Inactive.");
