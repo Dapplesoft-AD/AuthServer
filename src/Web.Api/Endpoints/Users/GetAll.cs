bb6aadee0545129cdfd109d4e9a78870c47230f5
Hasan Uddin <abcnizamd@gmail.com>
2025-12-03 13:02:00 +0600

    permission fixed ✅

--- src/Web.Api/Endpoints/Users/GetAll.cs
+++ src/Web.Api/Endpoints/Users/GetAll.cs
@@ -21,6 +21,7 @@ internal sealed class GetAll : IEndpoint
             return result.Match(Results.Ok, CustomResults.Problem);
         })
         .WithTags(Tags.Users)
+        .HasPermission(Permissions.UsersAccess)
         .RequireAuthorization();
     }
 }
