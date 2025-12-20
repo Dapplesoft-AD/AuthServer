ced58777769a60d38d9c74cd89cd7dec6552fda8
Hasan Uddin <abcnizamd@gmail.com>
2025-12-20 20:36:51 +0600

    ➕ Client Apps

---
+++ src/Application/ClientApps/Create/CreateClientCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Application.Abstractions.Messaging;
+using Application.Abstractions.Openiddict;
+using SharedKernel;
+using SharedKernel.Models;
+
+namespace Application.ClientApps.Create;
+
+public sealed record CreateClientCommandHandler(
+    IOpeniddictClientManager clientManager
+    ) : ICommandHandler<CreateClientCommand>
+{
+    async Task<Result> ICommandHandler<CreateClientCommand>.Handle(CreateClientCommand command, CancellationToken cancellationToken)
+    {
+
+        if (await clientManager.IsAlreadyExist(command.ClientId, cancellationToken))
+        {
+            return Result.Failure($"Client with ClientId '{command.ClientId}' already exists.");
+        }
+
+        await clientManager.CreateAsync(new ClientDescriptor
+        {
+            ClientId = command.ClientId,
+            DisplayName = command.DisplayName,
+            ClientSecret = command.ClientSecret,
+            RedirectUri = command.RedirectUri
+        }, cancellationToken);
+
+        return Result.Success();
+    }
+}
